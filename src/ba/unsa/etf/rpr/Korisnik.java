666addfa55afbe19846ab7a7ace386634bef7082
plvr99 <apalavra2@etf.unsa.ba>
2021-07-14 14:17:24 +0200

    Kreirana klasa za korisnika sa odgovarajucim getterima i setterima

---
+++ src/ba/unsa/etf/rpr/Korisnik.java
@@ -0,0 +1,27 @@
+package ba.unsa.etf.rpr;
+
+public class Korisnik {
+    private int id;
+    private String username;
+
+    public Korisnik(int id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
