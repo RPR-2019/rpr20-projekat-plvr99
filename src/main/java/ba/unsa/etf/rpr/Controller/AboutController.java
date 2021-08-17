400a923eaa3d88f82deae03fc10696c8577b4a12
plvr99 <apalavra2@etf.unsa.ba>
2021-08-17 13:53:18 +0200

    Izmijenjen izgled about prozora

--- src/main/java/ba/unsa/etf/rpr/Controller/AboutController.java
+++ src/main/java/ba/unsa/etf/rpr/Controller/AboutController.java
@@ -3,7 +3,7 @@ package ba.unsa.etf.rpr.Controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Hyperlink;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -16,8 +16,7 @@ import java.net.URISyntaxException;
 
 public class AboutController {
 
-    public Hyperlink webHyperlink;
-    public Hyperlink gitHyperlink;
+
     public Button closeBtn;
     public ImageView imgView;
     public GridPane grid;
@@ -25,27 +24,14 @@ public class AboutController {
     @FXML
     public void initialize(){
         grid.getStyleClass().add(JMetroStyleClass.BACKGROUND);
-        //Image image = new Image(getClass().getResourceAsStream("/images/logo.jpg"));
-       // imgView.setImage(image);
+        imgView.setImage(new Image("/images/logo_main.png"));
     }
     public void openGit(ActionEvent actionEvent) {
-        new Thread(()->{
+        new Thread(() -> {
             Desktop desktop = Desktop.getDesktop();
             if (desktop.isSupported(Desktop.Action.BROWSE)) {
                 try {
                     desktop.browse(new URI("https://github.com/plvr99"));
-                } catch (IOException | URISyntaxException ex) {
-                    ex.printStackTrace();
-                }
-            }
-        }).start();
-    }
-    public void openLink(ActionEvent actionEvent) {
-        new Thread(()->{
-            Desktop desktop = Desktop.getDesktop();
-            if (desktop.isSupported(Desktop.Action.BROWSE)) {
-                try {
-                    desktop.browse(new URI("https://www.etf.unsa.ba"));
                 } catch (IOException | URISyntaxException ex) {
                     ex.printStackTrace();
                 }
